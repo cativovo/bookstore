71990d3010128254b37d60959b82eec97be42e3d
Mark Leo Cativo <cativomarkleo@gmail.com>
2024-04-08 13:46:32 +0800

    chore: add seed to generate fake data in db

---
+++ backend/cmd/seed/main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/rand/v2"
+	"os"
+	"slices"
+	"sync"
+
+	"github.com/brianvoe/gofakeit/v7"
+	"github.com/cativovo/bookstore/pkg/book"
+	"github.com/cativovo/bookstore/pkg/storage/postgres"
+)
+
+const (
+	BOOK_COUNT  = 1000
+	GENRE_COUNT = 10
+)
+
+func main() {
+	connStr := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+	)
+	repository, err := postgres.NewPostgresRepository(connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var wg sync.WaitGroup
+
+	log.Println("seeding genres...")
+	genres := getGenres()
+	for _, genre := range genres {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			if err := repository.CreateGenre(genre); err != nil {
+				log.Printf("%s %s", genre, err)
+			}
+		}()
+	}
+	wg.Wait()
+	log.Println("seeding genres completed")
+
+	log.Println("seeding books...")
+	for range BOOK_COUNT {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			genreCount := rand.IntN(len(genres))
+			bookGenres := make([]string, 0, genreCount)
+
+			for len(bookGenres) != genreCount {
+				randomIndex := rand.IntN(len(genres))
+				genre := genres[randomIndex]
+				if !slices.Contains(bookGenres, genre) {
+					bookGenres = append(bookGenres, genre)
+				}
+			}
+
+			b := book.Book{
+				Title:       gofakeit.BookTitle(),
+				Author:      gofakeit.BookAuthor(),
+				Description: gofakeit.Product().Description,
+				CoverImage:  "https://placehold.co/600x400",
+				Price:       gofakeit.Price(0.99, 69.99),
+				Genres:      bookGenres,
+			}
+			repository.CreateBook(b)
+		}()
+	}
+
+	wg.Wait()
+	log.Println("seeding books completed")
+}
+
+func getGenres() []string {
+	genres := make([]string, 0, GENRE_COUNT)
+
+	for len(genres) != GENRE_COUNT {
+		genre := gofakeit.BookGenre()
+
+		if !slices.Contains(genres, genre) {
+			genres = append(genres, genre)
+		}
+	}
+
+	return genres
+}
--- backend/go.mod
+++ backend/go.mod
@@ -3,7 +3,9 @@ module github.com/cativovo/bookstore
 go 1.22.0
 
 require (
+	github.com/brianvoe/gofakeit/v7 v7.0.2
 	github.com/go-playground/validator/v10 v10.19.0
+	github.com/jackc/pgerrcode v0.0.0-20240316143900-6e2875d9b438
 	github.com/jackc/pgx/v5 v5.5.5
 	github.com/labstack/echo/v4 v4.11.4
 )
@@ -13,9 +15,9 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
-	github.com/jackc/pgerrcode v0.0.0-20240316143900-6e2875d9b438 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
@@ -24,6 +26,7 @@ require (
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.19.0 // indirect
 	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.17.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
